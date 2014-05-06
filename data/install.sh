f6de404a37b2aec8644ac9155e2f846cba45c4fd
Nathan Isom <nathanisom27@gmail.com>
2014-05-05 19:55:58 -0500

    Changed install to use wget.

--- data/install.sh
+++ data/install.sh
@@ -1,15 +1,15 @@
 #Meant to be used to configure blank system with current packages and dotfiles.
 sudo pacman -Syu --noconfirm
-sudo pacman -S sudo git base-devel abs file expac --noconfirm
+sudo pacman -S wget sudo git base-devel abs file expac --needed --noconfirm
 
 #install pacaur:
 #build cower and install:
-curl https://aur.archlinux.org/packages/co/cower/cower.tar.gz
+wget http://aur.archlinux.org/packages/co/cower/cower.tar.gz
 tar xzf cower.tar.gz
 cd cower
 makepkg -i
 cd ..
-curl https://aur.archlinux.org/packages/pa/pacaur/pacaur.tar.gz
+wget http://aur.archlinux.org/packages/pa/pacaur/pacaur.tar.gz
 cd pacaur
 makepkg -i
 cd ..
