8485ddd6dc039c43b780365cf753b7ef225b41d2
neeasade <nathanisom27@gmail.com>
2015-08-12 04:34:59 -0500

    tiny

--- data/install.sh
+++ data/install.sh
@@ -1,5 +1,5 @@
 #Meant to be used to configure blank system with current packages and dotfiles.
-pacman -S git
+pacman -S git --noconfirm
 git clone https://github.com/neeasade/AutoArch install
 cd install
 ./config.sh
