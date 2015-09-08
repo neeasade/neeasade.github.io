20fac271e03ac8ee640ee3088000274a69086289
neeasade <nathanisom27@gmail.com>
2015-09-08 13:04:04 -0500

    checkpoint, still getting the hang of routing/views. not functional.

--- public/js/jekyllapps.js
+++ public/js/jekyllapps.js
@@ -7,24 +7,51 @@
  * Inject required $objects into our Controller
  */
 
-// not conflict with jekyll bindings.
 angular
-  .module('jekyllApp', [])
-  .controller('ctrl', ctrl)
-  .config(function($interpolateProvider){
-    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-    })
+    .module('jekyllApp', ['ngRoute'])
+    .controller('ctrl', ctrl)
+
+    // not conflict with jekyll bindings.
+    .config(function($interpolateProvider){
+        $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+        })
+
+    .config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+            when('/p:postId', {
+                controller: 'ctrl',
+                template: ' {[{  content }]} '
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+        }])
 ;
 
-function ctrl($scope, $http) {
+function ctrl($scope, $http, $routeParams) {
+  $scope.postId = $routeParams.postId;
   $scope.posts = [];
+  $scope.content = [];
+
+  $http
+    .get($scope.posts[postId].url)
+    .then(setContent);
+
+  function setContent(result) {
+    $scope.content=result;
+  }
 
   $http
     .get('/site.json')
-    .then(onSuccess)
-  ;
+    .then(onSuccess);
 
   function onSuccess(result) {
     $scope.posts = result.data;
   }
 }
+
+// two controllers, one for index, one for content,
+// the second will make http request for cnotent based on routeParam array from link.
+
+
