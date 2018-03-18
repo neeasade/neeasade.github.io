4b4998e72b804f5d4425b6b2815bc9da47917b55
neeasade <nathanisom27@gmail.com>
2018-03-18 14:22:37 -0500

    todone

--- assets/img/desktops/make_thumbs.sh
+++ assets/img/desktops/make_thumbs.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# todo here: clear out the thumb folder 
-# also regen the list on the desktop page so there isn't a disconnect there
-
 # clear desktop list
 sed -i '/^desktops/,/^---/d' "../../../pages/desktop.html"
 sed -i "/permalink:/a ---" "../../../pages/desktop.html"
