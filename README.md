63861f6569df27d595220019aed4f59005491355
neeasade <neeasade@gmail.com>
2019-03-09 11:56:35 -0500

    getting there

--- README.md
+++
@@ -1,6 +0,0 @@
-## notes.neeasade.net
-
-org mode blog, generated with org-static-blog [1], and sakura [2].
-
-[1] https://github.com/bastibe/org-static-blog/
-[2] https://github.com/oxalorg/sakura
--- site/assets/img/desktops/make_thumbs.sh
+++ site/assets/img/desktops/make_thumbs.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
-# clear desktop list
-sed -i '/^desktops/,/^---/d' "../../../pages/desktop.html"
-sed -i "/permalink:/a ---" "../../../pages/desktop.html"
-sed -i "/permalink:/a desktops:" "../../../pages/desktop.html"
+cd $(dirname $([ -L $0  ] && readlink -f $0 || echo $0))
 
-# clear thumbs
-rm thumb/*
+sed -i "/{{{.*/d" "../../../../pages/desktop.org"
 
 for file in $(find full -type f | sort); do
-  convert $file -resize 900x -filter Lanczos thumb/$(basename $file) 
+  filename="$(basename $file)"
+  [ ! -f "thumb/$filename" ] && \
+      convert "$file" -resize 900x -filter Lanczos "thumb/$filename"
 
-  # insert the line at the top of desktops
-  filename="$(basename $file | sed 's/.png//')"
-  echo $filename
-  sed -i "/desktops:/a \    - $filename" "../../../pages/desktop.html"
+  echo "{{{dtop($filename)}}}" >> "../../../../pages/desktop.org"
 done
-
