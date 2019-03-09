54e36e18ca2d9f0c6a4c2a8bf15461668769bf08
neeasade <neeasade@gmail.com>
2019-03-09 11:57:44 -0500

    rename

--- site/assets/img/desktops/make_thumbs.sh
+++ site/assets/img/desktops/make_thumbs.sh
@@ -2,12 +2,12 @@
 
 cd $(dirname $([ -L $0  ] && readlink -f $0 || echo $0))
 
-sed -i "/{{{.*/d" "../../../../pages/desktop.org"
+sed -i "/{{{.*/d" "../../../../pages/rice.org"
 
 for file in $(find full -type f | sort); do
   filename="$(basename $file)"
   [ ! -f "thumb/$filename" ] && \
       convert "$file" -resize 900x -filter Lanczos "thumb/$filename"
 
-  echo "{{{dtop($filename)}}}" >> "../../../../pages/desktop.org"
+  echo "{{{dtop($filename)}}}" >> "../../../../pages/rice.org"
 done
