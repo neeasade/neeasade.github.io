bea4b07fbc86dae8b8d74b98cafe69cff88f0031
neeasade <neeasade@gmail.com>
2019-03-09 13:19:50 -0500

    thumb bug

--- site/assets/img/desktops/make_thumbs.sh
+++ site/assets/img/desktops/make_thumbs.sh
@@ -4,8 +4,9 @@ cd $(dirname $([ -L $0  ] && readlink -f $0 || echo $0))
 
 sed -i "/{{{.*/d" "../../../../pages/rice.org"
 
-for file in $(find full -type f | sort); do
-  filename="$(basename $file)"
+find full -type f | sort | tac | while read -r file; do
+  filename="$(basename "$file")"
+  echo $filename
   [ ! -f "thumb/$filename" ] && \
       convert "$file" -resize 900x -filter Lanczos "thumb/$filename"
 
