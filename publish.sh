06bc5d06e7b4e1509d1a6e1863dc1674140abd57
neeasade <neeasade@gmail.com>
2020-01-23 18:57:47 -0500

    qt faces, dirty tree handling

--- publish.sh
+++ publish.sh
@@ -4,10 +4,16 @@
 cd "$(dirname $([ -L $0  ] && readlink -f $0 || echo $0))"
 set -e
 
+dirty_tree=$(iif 'git status | grep -q "working tree clean"')
+
 vdo() {
     echo "$*"
     "$@"
 }
+
+if $dirty_tree; then
+    vdo git stash
+fi
 
 export elisp_timeout=30
 vdo elisp "(ns/blog-generate)"
@@ -23,3 +29,7 @@ vdo git add --all
 vdo git commit -m "auto commit publish.sh"
 vdo git push origin master
 vdo git checkout source
+
+if $dirty_tree; then
+    vdo git stash pop
+fi
