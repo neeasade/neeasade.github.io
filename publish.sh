92e211cc9ad9f70f2783a68e366a0807d16a54e2
neeasade <neeasade@gmail.com>
2020-04-07 09:46:50 -0500

    set -x

--- publish.sh
+++ publish.sh
@@ -6,39 +6,33 @@ set -e
 
 clean_tree=$(iif 'git status | grep -q "working tree clean"')
 
-vdo() {
-    echo "\$ $*"
-    if ! "$@"; then
-	echo "!!! step failed"
-	exit 1
-    fi
-}
+set -x
 
 if ! $clean_tree; then
-    vdo git stash
+    git stash
 fi
 
 export elisp_timeout=30
-vdo elisp "(ns/blog-generate)"
+elisp "(ns/blog-generate)"
 
-vdo rm -rf /tmp/notes.neeasade.net
-vdo cp -r site /tmp/notes.neeasade.net
+rm -rf /tmp/notes.neeasade.net
+cp -r site /tmp/notes.neeasade.net
 
-vdo git reset --hard
+git reset --hard
 
-vdo git checkout master
+git checkout master
 
 git fetch origin
 git reset --hard origin/master
 
-vdo rm -rf *
-vdo cp -r /tmp/notes.neeasade.net/* ./
+rm -rf *
+cp -r /tmp/notes.neeasade.net/* ./
 
-vdo git add --all
-vdo git commit -m "auto commit publish.sh"
-vdo git push origin master
-vdo git checkout source
+git add --all
+git commit -m "auto commit publish.sh"
+git push origin master
+git checkout source
 
 if ! $clean_tree; then
-    vdo git stash pop
+    git stash pop
 fi
