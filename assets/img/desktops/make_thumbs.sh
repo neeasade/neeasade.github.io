b846d3cb30063036885cbe057252e8fca845a10f
neeasade <neeasade@users.noreply.github.com>
2025-06-18 14:50:13 -0500

    css sync

--- assets/img/desktops/make_thumbs.sh
+++ assets/img/desktops/make_thumbs.sh
@@ -8,6 +8,6 @@ find full -type f | sort | tac | while read -r file; do
   filename="$(basename "$file")"
   if [ ! -f "thumb/$filename" ]; then
       echo $filename
-      convert "$file" -resize 900x -filter Lanczos "thumb/$filename"
+      magick "$file" -resize 900x -filter Lanczos "thumb/$filename"
   fi
 done
