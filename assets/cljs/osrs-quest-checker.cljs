b198a319c1eae293ed5d0d7037b16ec900a88a6e
neeasade <neeasade@users.noreply.github.com>
2025-08-09 16:13:57 -0500

    checkmarks

--- assets/cljs/osrs-quest-checker.cljs
+++ assets/cljs/osrs-quest-checker.cljs
@@ -33,7 +33,7 @@
         url (str (.. js/window -location -protocol) "//" (.. js/window -location -host) (.. js/window -location -pathname)
                  "?"
                  (.  (js/URLSearchParams. (clj->js save-state)) toString))]
-    (. (. js/window -history) replaceState
+    (. (. js/window -history) pushState
        (clj->js {:path url})
        "" url)
     (set! (. js/document -title) (str (:quest @state) "- osrs quest checker"))))
@@ -59,6 +59,7 @@
                                    (reset! search-term ""))}
                item])])]))))
 
+(declare update-blockers!)
 (defn blockers [player-data quest-name]
   (let [player-data (assoc-in player-data [:levels :questpoints] 400) ; faking/not handling for now
         want (first (filter #(= (:title %) quest-name) quest-data))
@@ -67,20 +68,30 @@
         missing-quests (keep (fn [dep-name]
                                ;; 0 not started, 1 started, 2 done
                                (when (not (= 2 ((keyword dep-name) (:quests player-data))))
-                                 (str "Quest: " dep-name)))
+                                 ;; (str "Quest: " dep-name)
+                                 [:span "Quest: "
+                                  [:a {:href "javascript:;"
+                                       :on-click #(do (swap! state assoc :quest dep-name)
+                                                      (update-blockers!)
+                                                      )}
+                                   (if (empty? (blockers player-data
+                                                         dep-name))
+                                     (str dep-name " ✅")
+                                     dep-name)]]))
                              ;; bad data
                              (->> (:quests want)
                                   distinct
-                                  (remove (partial = "Kudos"))))
+                                  (remove (partial = "Kudos")))
+                             )
         missing-skills (->> (:skills want)
                             (remove (fn [[skill level]]
                                       (>= (skill (:levels player-data))
                                           level)))
-                            (map (fn [[s l]] (format "%s/%s %s"
-                                                     (-> player-data :levels s)
-                                                     l
-                                                     (name s)
-                                                     ))))]
+                            (map (fn [[s l]]
+                                   [:span
+                                    (str (-> player-data :levels s) "/")
+                                    [:b (str l " " (name s))]]
+                                   )))]
     (concat missing-skills missing-quests)))
 
 
@@ -133,16 +144,15 @@
 (defn update-quest-optimal []
   (let [players (keys (:players @state))]
     (swap! state assoc :quest
-           (or
-            (->> optimal-quest-list
-                 (some
-                  (fn [quest]
+           (or (->> optimal-quest-list
                     (some
-                     (fn [player]
-                       (when (not (= 2 (or (get-in @state [:players player :quests (keyword quest)]) 2)))
-                         quest)) ; comp for extra stuff we don't have in quest data
-                     players))))
-            "None")))
+                     (fn [quest]
+                       (some
+                        (fn [player]
+                          (when (not (= 2 (or (get-in @state [:players player :quests (keyword quest)]) 2)))
+                            quest)) ; comp for extra stuff we don't have in quest data
+                        players))))
+               "None")))
   (update-blockers!))
 
 (defn update-quest-doable []
@@ -165,6 +175,7 @@
             "None")))
   (update-blockers!))
 
+
 (defn my-component []
   [:div
    [:input {:type "text"
@@ -177,8 +188,7 @@
    (when-let [e (:error @state)] [:h2 e])
    [:p "Selected Quest: " [:b
                            [:a {:href (str "https://oldschool.runescape.wiki/w/" (string/replace (:quest @state) " " "_"))}
-                            (:quest @state)]]
-    ]
+                            (:quest @state)]]]
    [(search-box (map :title quest-data)
                 #(do (swap! state assoc :quest %)
                      (update-blockers!)))]
@@ -193,11 +203,20 @@
                       (str player " is ready! ✅")
                       (str player " is missing:"))]
           (when-not (empty? blockers)
-            [:ul (for [line blockers] [:li line])])])))])
+            [:ul (for [line blockers]
+                   [:li line])])])))])
 
 (when-let [url-state (parse-url)]
   (swap! state merge url-state)
   (update-player-stats! (:player-string url-state))
   (update-blockers!))
+
+(.addEventListener
+ js/window "popstate"
+ (fn [e]
+   (when-let [url-state (parse-url)]
+     (swap! state merge url-state)
+     ;; (update-player-stats! (:player-string url-state))
+     (update-blockers!))))
 
 (rdom/render [my-component] (.getElementById js/document "app"))
